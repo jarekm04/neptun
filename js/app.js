439dc4fff931cd50891f3757b2e3955810315db6
Jarek <jarek_m94@wp.pl>
2022-04-13 12:17:58 +0200

    prevent def

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 //logo scrolls to top
 const logoBtn = document.querySelector(".header__logo");
 
-logoBtn.addEventListener("click", () => {
+logoBtn.addEventListener("click", (e) => {
+    e.preventDefault();
     window.scroll({ top: 0, left: 0, behavior: 'smooth' });
 })
