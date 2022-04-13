f3555b92e127973eea82aa49d9eca10843323993
Jarek <jarek_m94@wp.pl>
2022-04-13 15:43:08 +0200

    nav mobile hamburger menu

--- js/app.js
+++ js/app.js
@@ -4,4 +4,17 @@ const logoBtn = document.querySelector(".header__logo");
 logoBtn.addEventListener("click", (e) => {
     e.preventDefault();
     window.scroll({ top: 0, left: 0, behavior: 'smooth' });
-})
+});
+
+//mobile hamburger menu
+const hamburgerIcon = document.querySelector(".hamburger");
+const menuList = document.querySelector(".header__menu");
+
+hamburgerIcon.addEventListener("click", () => {
+    menuList.classList.toggle("active");
+    menuList.classList.contains("active") ? hamburgerIcon.textContent = "cancel" : hamburgerIcon.textContent = "menu";
+});
+document.querySelectorAll(".menu__item").forEach( item => item.addEventListener("click", () => {
+    menuList.classList.remove("active");
+    menuList.classList.contains("active") ? hamburgerIcon.textContent = "cancel" : hamburgerIcon.textContent = "menu";
+}))
