a20e75bedb1ca54eb2408f16db7c1f8301f2535c
Jarek <jarek_m94@wp.pl>
2022-04-14 16:54:45 +0200

    scss update, transitions between pages in progress

--- js/app.js
+++ js/app.js
@@ -10,3 +10,26 @@ document.querySelectorAll(".menu__item").forEach( item => item.addEventListener(
     menuList.classList.remove("active");
     menuList.classList.contains("active") ? hamburgerIcon.textContent = "cancel" : hamburgerIcon.textContent = "menu";
 }))
+
+//pages transitions
+window.onload = () => {
+    const transitionEl = document.querySelector('.transition');
+    const anchors = document.querySelectorAll('.header__menu li a');
+
+    setTimeout(() => {
+        transitionEl.classList.remove('active');
+    }, 500);
+
+    anchors.forEach((anchor) => {
+        anchor.addEventListener('click', e => {
+            e.preventDefault();
+            let target = e.target.href;
+
+            transitionEl.classList.add('active');
+
+            setTimeout(() => {
+                window.location.href = target;
+            }, 500);
+        });
+    });
+}
