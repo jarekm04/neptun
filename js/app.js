97eb3db9ffee33b6c43958c05268fadf7d9ce4d8
Jarek <jarek_m94@wp.pl>
2022-04-14 20:58:44 +0200

    photos order changed

--- js/app.js
+++ js/app.js
@@ -10,3 +10,20 @@ document.querySelectorAll(".menu__item").forEach( item => item.addEventListener(
     menuList.classList.remove("active");
     menuList.classList.contains("active") ? hamburgerIcon.textContent = "cancel" : hamburgerIcon.textContent = "menu";
 }))
+
+//trainers popup
+const trainersImgs = document.querySelectorAll(".trainer__photo");
+const popup = document.querySelector(".popup");
+const popupClose = document.querySelector(".popup__close");
+const popupImg = document.querySelector(".popup__img");
+
+trainersImgs.forEach((img) => {
+    img.addEventListener("click", (e) => {
+        popup.classList.remove("hidden");
+        popupImg.src = e.target.src;
+    });
+})
+
+popupClose.addEventListener("click", () => {
+    popup.classList.add("hidden");
+});
