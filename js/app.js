f9f21efe3fcebeb1975f34bdb784af6bb59defae
Jarek <jarek_m94@wp.pl>
2022-04-14 21:33:13 +0200

    works with gallery, next, prev, trainers bio

--- js/app.js
+++ js/app.js
@@ -16,11 +16,27 @@ const trainersImgs = document.querySelectorAll(".trainer__photo");
 const popup = document.querySelector(".popup");
 const popupClose = document.querySelector(".popup__close");
 const popupImg = document.querySelector(".popup__img");
+const trainerBIO = document.querySelector(".trainer__bio");
+const arrowLeft = document.querySelector(".popup__arrow--left");
+const arrowRight = document.querySelector(".popup__arrow--right");
+
+let currentImgIndex;
+
+// const showNextImg = () => {
+//     if (currentImgIndex === trainersImgs.length - 1) {
+//         currentImgIndex = 0;
+//     } else {
+//         currentImgIndex++;
+//     }
+//     console.log(trainersImgs);
+//     // pop.src = trainersImgs[currentImgIndex].src;
+// };
 
 trainersImgs.forEach((img) => {
     img.addEventListener("click", (e) => {
         popup.classList.remove("hidden");
         popupImg.src = e.target.src;
+        trainerBIO.textContent = "";
     });
 })
 
