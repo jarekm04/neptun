3f3ad6e9c3c89567d91bb347b9412a069b1ec240
Jarek <jarek_m94@wp.pl>
2022-05-04 14:20:34 +0200

    deleted nav swiper from theorypages, added some fixes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+//--------------------------AOS fade in sections--------------------------------------------
+AOS.init();
+
 import { db } from "../firebase.js";
 import { doc, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
 
@@ -14,38 +17,12 @@ document.querySelectorAll(".menu__item").forEach( item => item.addEventListener(
     menuList.classList.contains("active") ? hamburgerIcon.textContent = "cancel" : hamburgerIcon.textContent = "menu";
 }))
 
-//----------------------------swiper theory carousel--------------------------------------------
-const $theory = document.querySelector(".theory");
-let $theoryContentClassList;
-
-if ($theory.classList.contains("theoryPages")) {
-    $theoryContentClassList = [...document.querySelector(".theory-content").classList];
-}
-
-const handleInitialSlide = (sectionClasses) => {
-    if (sectionClasses.includes("history")) {
-        return 0;
-    } else if (sectionClasses.includes("secrets")) {
-        return 1;
-    } else if (sectionClasses.includes("techniques")) {
-        return 2;
-    } else if (sectionClasses.includes("fight")) {
-        return 3;
-    } else if (sectionClasses.includes("selfDefense")) {
-        return 4;
-    } else if (sectionClasses.includes("arrangements")) {
-        return 5;
-    } else if (sectionClasses.includes("hardening")) {
-        return 6;
-    }
-}
-
 const swiper = new Swiper(".mySwiper", {
     effect: "coverflow",
     grabCursor: true,
     centeredSlides: true,
     slidesPerView: "auto",
-    initialSlide: $theory.classList.contains("theoryPages") ? handleInitialSlide($theoryContentClassList) : 3,
+    initialSlide: 3,
     coverflowEffect: {
         rotate: 50,
         stretch: 0,
@@ -61,9 +38,6 @@ const swiper = new Swiper(".mySwiper", {
         el: ".swiper-pagination",
     },
 });
-
-//--------------------------AOS fade in sections--------------------------------------------
-AOS.init();
 
 //--------------------------firebase get news-----------------------------------------------
 const colRef = collection(db, "News");
